3ff46485e93b8192624fb19a4fd34196711dc1d1
DESKTOP-TB7US0M\나 <rnrndkz@gmail.com>
2023-04-18 01:15:46 +0900

    Initial-commit,

---
+++ Assets/Player.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerController))]
+public class Player : MonoBehaviour
+{
+    public float jumpHeight = 4;
+    public float timeToJumpApex = .4f;
+
+    public float accelerationTimeGrounded = .1f;
+    public float moveSpeed = 6;
+
+    float gravity;
+    float jumpVelocity;
+    Vector3 velocity;
+    float velocityXSmoothing;
+
+    Vector2 inputVector;
+    PlayerController controller;
+
+    private void Start() {
+        controller = GetComponent<PlayerController>();
+
+        gravity = -(2 * jumpHeight) / Mathf.Pow(timeToJumpApex, 2);
+        jumpVelocity = Mathf.Abs(gravity) * timeToJumpApex;
+    }
+
+    private void Update() {
+        if(controller.info.above || controller.info.below)
+        {
+            velocity.y = 0;
+        }
+        inputVector.x = Input.GetAxisRaw("Horizontal");
+        inputVector.y = Input.GetAxisRaw("Vertical");
+
+        float targetVelocityX = inputVector.x * moveSpeed;
+        velocity.x = Mathf.SmoothDamp(velocity.x, targetVelocityX, ref velocityXSmoothing, accelerationTimeGrounded);
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : RaycastController
+{
+    public CollisionInfo info;
+    public Vector3 velocityOld;
+
+    public override void Start()
+    {
+        base.Start();
+    }
+
+    public void Move(Vector3 velocity)
+    {
+        UpdateRaycastOrigins();
+        info.Reset();
+        velocityOld = velocity;
+
+        if(velocity.y != 0)
+        {
+            VerticalCollision(ref velocity);
+        }
+
+        transform.Translate(velocity);
+    }
+
+    void VerticalCollision(ref Vector3 velocity)
+    {
+        float directionY = Mathf.Sign(velocity.y);
+        float rayLength = Mathf.Abs(velocity.y) + skinWidth;
+        for (int i = 0; i < verticalRayCount; i++)
+        {
+            Vector2 rayOrigin = (directionY == -1) ? raycastOrigins.bottomLeft : raycastOrigins.topLeft;
+            rayOrigin += Vector2.right * (verticalRaySpacing * i + velocity.x);
+            RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.up * directionY, rayLength, collisionMask);
+            if(hit)
+            {
+                velocity.y = (hit.distance - skinWidth) * directionY;
+                rayLength = hit.distance;
+
+                info.below = directionY == -1;
+                info.above = directionY == 1;
+            }
+        }
+    }
+
+
+    [System.Serializable]
+    public struct CollisionInfo
+    {
+        public bool above, below, left, right;
+
+        public float slopeAngle, slopeAngleOld;
+
+        public void Reset()
+        {
+            above = below = left = right = false;
+
+            slopeAngleOld = slopeAngle;
+            slopeAngle = 0;
+        }
+    }
+}
